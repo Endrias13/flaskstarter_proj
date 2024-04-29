aac213d1b89e82419e232dc763839247674b99c3
Endrias13 <33791240+Endrias13@users.noreply.github.com>
2024-04-29 11:19:36 -0400

    Update command_Task_1.sh

--- command_Task_1.sh
+++ command_Task_1.sh
@@ -5,3 +5,17 @@ python3 -m venv ~/.myrepo
 source ~/.myrepo/bin/activate
 
 make -f Makefile_Task_1 all
+
+git clone git@github.com:Endrias13/flaskstarter_proj.git
+
+python3 -m venv ~/.flask-ml-azure
+
+source ~/.flask-ml-azure/bin/activate
+
+pip install -r requirements.txt 
+
+az webapp up -n flaskapp-proj-app 
+
+chmod +x make_predict_azure_app.sh
+
+./make_predict_azure_app.sh
